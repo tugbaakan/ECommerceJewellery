c95b0b9ecb53b2d2e95f24ef952b8fc61f629b86
Tugba Akan <akan.tugba@gmail.com>
2020-12-13 19:49:32 +0300

    Update Carty.cs

--- API/Entities/Carty.cs
+++ API/Entities/Carty.cs
@@ -2,6 +2,7 @@ namespace API.Entities
 {
     public class Carty
     {
+        /* this class represent every item in the cart with its quantity */
         public int Id { get; set; }
         public int CartId {get; set;}
         public int ProductId {get; set;}
